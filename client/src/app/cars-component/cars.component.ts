4494aefa8a92264c2a31a720557d30cf3abb9043
Vladimir Georgiev <vladiggeorgiev@gmail.com>
2021-08-01 14:19:00 +0300

    Fixed the UI to look better. Added launch.json

--- client/src/app/cars-component/cars.component.ts
+++ client/src/app/cars-component/cars.component.ts
@@ -7,19 +7,22 @@ import { ApiService } from '../api.service';
   styleUrls: ['./cars.component.css']
 })
 export class CarsComponent implements OnInit {
-
+  loading: boolean = false;
+  numberOfCars: number = 0;
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
 
   uploadFile(event: any) {
+    this.loading = true;
     let file = event.target.files[0];
     let formData = new FormData();
     formData.append('file', file, file.name);
 
     this.apiService.uploadFile(formData).subscribe(data => {
-      console.log(data)
+      this.numberOfCars = data;
+      this.loading = false;
     })
   }
 }
